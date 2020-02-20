69a45f49a0071387572499e47e33b826e7bef01a
Ed <ygraik@protonmail.com>
2020-02-20 20:01:26 +0100

    completed libraryclass and fixed it

--- libraryClass.py
+++ libraryClass.py
@@ -12,6 +12,16 @@ class HashCode:
 	def addLibrary(self, lib):
 		self.libraries.append(lib)
 
+	def removeBookFromEveryLibs(self, bookId):
+		for l in self.libraries:
+			l.removeBook(bookId)
+
+	def __str__(self):
+		output = f"Time limit = {self.timeLimit}"
+		for l in self.libraries:
+			output += str(l) + "\n"
+		return output
+
 	def __init__(self, numOfLibraries):
 		self.numOfLibraries = numOfLibraries
 	
@@ -20,19 +30,23 @@ class LibraryClass:
 	timeToSignup = 0
 	books = []
 	numOfBooks = 0
+	maxBooksPerDay = 0
 	libId = 0
+
+	def removeBook(self, bookId):
+		for b in self.books:
+			if b.bookId == bookId:
+				self.books.remove(b)
 
 	def addBook(self, bookId, score):
 		self.books.append(Book(bookId, score))
 
+	def addBook(self, book):
+		self.books.append(book)
+
 	def check(self):
 		return len(self.books) == self.numOfBooks
 	
-	def __init__(self, numOfBooks, libId, timeToSignup):
-		self.numOfBooks = numOfBooks
-		self.libId = libId
-		self.timeToSignup = timeToSignup
-
 	def getTotalValue(self):
 		total = 0
 		for b in self.books:
@@ -48,10 +62,25 @@ class LibraryClass:
 				bookId = b.bookId
 		return bookId, score
 
+	def __str__(self):
+		books = ""
+		for b in self.books:
+			books += str(b)
+		return f"Id :{self.libId}, time: {self.libId}, max/day: {self.maxBooksPerDay} -> " + books
+
+	def __init__(self, numOfBooks, libId, timeToSignup, maxBooksPerDay):
+		self.numOfBooks = numOfBooks
+		self.libId = libId
+		self.timeToSignup = timeToSignup
+		self.maxBooksPerDay = maxBooksPerDay
+
 
 class Book:
 	bookId = 0
 	score = 0
+
+	def __str__(self):
+		return f"({self.bookId}:{self.score})"
 
 	def __init__(self, bookId, score):
 		self.bookId = bookId
