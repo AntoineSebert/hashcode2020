0211f1ba55ca244d19d0e4e21e1efe5592f351f9
Ed <ygraik@protonmail.com>
2020-02-20 19:32:18 +0100

    improved class and fixed missing self

--- libraryClass.py
+++ libraryClass.py
@@ -3,10 +3,21 @@
 
 class HashCode:
 	timeLimit = 0
+	numOfLibraries = 0
 	libraries = []
+
+	def check(self):
+		return len(libraries) == numOfLibraries
+
+	def addLibrary(self, lib):
+		self.libraries.append(lib)
+
+	def __init__(self, numOfLibraries):
+		self.numOfLibraries = numOfLibraries
 	
 
 class LibraryClass:
+	timeToSignup = 0
 	books = []
 	numOfBooks = 0
 	libId = 0
@@ -15,10 +26,27 @@ class LibraryClass:
 		self.books.append(Book(bookId, score))
 
 	def check(self):
-		return len(books) == numOfBooks
+		return len(self.books) == self.numOfBooks
 	
-	def __init__(self, numOfBooks, libId):
+	def __init__(self, numOfBooks, libId, timeToSignup):
 		self.numOfBooks = numOfBooks
+		self.libId = libId
+		self.timeToSignup = timeToSignup
+
+	def getTotalValue(self):
+		total = 0
+		for b in self.books:
+			total += b.score
+		return total
+
+	def getMostValuable(self):
+		score = 0
+		bookId = 0
+		for b in self.books:
+			if score < b.score:
+				score = b.score
+				bookId = b.bookId
+		return bookId, score
 
 
 class Book:
